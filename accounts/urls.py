231a05456138e02c004c19bda8c9232cd1c464b4
anshul bhardwaj <ratulroyal5@gmail.com>
2024-02-01 19:56:56 +0530

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from accounts import views
+
+urlpatterns = [
+
+
+    path('demo',views.home,name='home'),
+    path('signup', views.signup, name='signup'),
+    path('login',views.signin,name='login'),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    path('logout',views.logout_view,name='logout'),
+    path('reset_password',views.forgot_password,name='reset_password')
+
+
+]
